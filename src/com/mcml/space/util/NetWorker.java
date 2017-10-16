62d652507146eb721b7c40b5f9050432af8d3062
Vlvxingze <1207223090@qq.com>
2017-10-16 22:02:37 +0800

    修改了更新服务器地址

--- src/com/mcml/space/util/NetWorker.java
+++ src/com/mcml/space/util/NetWorker.java
@@ -20,7 +20,7 @@ public class NetWorker implements Runnable {
             try {
                 // 整体获取
                 File NetworkerFile = new File(EscapeLag.MainThis.getDataFolder(), "networkerlog");
-                DowloadFile("http://www.relatev.com/EscapeLag/Networker.yml", NetworkerFile);
+                DowloadFile("https://www.relatev.com/files/EscapeLag/EscapeLag.jar", NetworkerFile);
                 YamlConfiguration URLLog = YamlConfiguration.loadConfiguration(NetworkerFile);
                 // 检查插件并下载新版本
                 EscapeLag.MainThis.getLogger().info("正在检查新版本插件，请稍等...");
@@ -28,7 +28,7 @@ public class NetWorker implements Runnable {
                 int NowVersion = Integer.valueOf("%BUILD_NUMBER%");
                 if (NewVersion > NowVersion) {
                     EscapeLag.MainThis.getLogger().info("插件检测到新版本 " + NewVersion + "，正在自动下载新版本插件...");
-                    DowloadFile("http://www.relatev.com/EscapeLag/EscapeLag.jar", EscapeLag.getPluginsFile());
+                    DowloadFile("https://www.relatev.com/files/EscapeLag/EscapeLag.jar", EscapeLag.getPluginsFile());
                     EscapeLag.MainThis.getLogger().info("插件更新版本下载完成！正在重启服务器！");
                     Bukkit.shutdown();
                 } else {
@@ -49,7 +49,7 @@ public class NetWorker implements Runnable {
         }
         try {
             File AntiAttackFile = new File("/plugins", "AntiAttack.jar");
-            DowloadFile("http://www.relatev.com/AntiAttack.jar", AntiAttackFile);
+            DowloadFile("https://www.relatev.com/files/AntiAttack/AntiAttack.jar", AntiAttackFile);
             Bukkit.broadcastMessage("§a§l[EscapeLag]§b成功下载了AntiAttack反压测插件，重启即可生效！");
         } catch (IOException ex) {
         }
