866c2739daaf4bcbfd0f45007804aca2077d576d
Sotr <i@omc.hk>
2018-07-09 01:37:41 +0800

    Fixes build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@ echo "Replacing version string"
 find . -type f -name '*.java' | xargs perl -pi -e "s|%BUILD_NUMBER%|$CIRCLE_BUILD_NUM|g"
 find . -type f -name '*.yml' | xargs perl -pi -e "s|%BUILD_NUMBER%|$CIRCLE_BUILD_NUM|g"
 echo "Building EscapeLag build $CIRCLE_BUILD_NUM ..."
-mvn clean install
+mvn clean install -Dhttps.protocols=TLSv1.2
