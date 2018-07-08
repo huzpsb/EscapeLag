edf0a2a5ac5319dabf4a071ec45b34c598de8842
Sotr <i@omc.hk>
2018-07-09 04:17:20 +0800

    Cleanup #2

--- src/main/java/com/mcml/space/function/FarmProtecter.java
+++ src/main/java/com/mcml/space/function/FarmProtecter.java
@@ -4,6 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityInteractEvent;
@@ -12,28 +13,23 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import com.mcml.space.config.ConfigFunction;
 
 public class FarmProtecter implements Listener {
-    // TODO be graceful
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void EntityFarmChecker(EntityInteractEvent event){
-        if(ConfigFunction.ProtectFarmenable){
-            if(event.getEntityType() != EntityType.PLAYER){
-                Block block = event.getBlock();
-                if(block.getType() == Material.SOIL||block.getType() == Material.CROPS){
-                    event.setCancelled(true);
-                }
-            }
+        if(!ConfigFunction.ProtectFarmenable || event.getEntityType() == EntityType.PLAYER) return; // TODO 细分实体与玩家
+        
+        Block block = event.getBlock();
+        if(block.getType() == Material.SOIL || block.getType() == Material.CROPS){
+            event.setCancelled(true);
         }
     }
-
-    @EventHandler
+    
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void PlayerFarmChecker(PlayerInteractEvent event){
-        if(ConfigFunction.ProtectFarmenable){
-            if(event.getAction() == Action.PHYSICAL){
-                Block block = event.getClickedBlock();
-                if(block.getType() == Material.SOIL||block.getType() == Material.CROPS){
-                    event.setCancelled(true);
-                }
-            }
+        if(!ConfigFunction.ProtectFarmenable || event.getAction() != Action.PHYSICAL) return;
+        
+        Block block = event.getClickedBlock();
+        if(block.getType() == Material.SOIL || block.getType() == Material.CROPS){
+            event.setCancelled(true);
         }
     }
 }
