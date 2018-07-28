4aa383335c3a9cad9292acbde2e6fc1e5720bc81
Vlvxingze <1207223090@qq.com>
2018-07-28 16:49:40 +0800

    还是搞回去吧

--- src/main/java/com/mcml/space/core/PlayerList.java
+++ src/main/java/com/mcml/space/core/PlayerList.java
@@ -5,18 +5,32 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerList implements Listener{
+    private static List<Player> Players = new ArrayList();
     
     public static void init(){
+        for(World world:Bukkit.getWorlds()){
+            Players.addAll(world.getPlayers());
+        }
+        Bukkit.getPluginManager().registerEvents(new PlayerList(), EscapeLag.plugin);
+    }
+    
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Players.add(event.getPlayer());
+    }
+    
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event){
+        Players.remove(event.getPlayer());
     }
     
     public static List<Player> getPlayerList(){
-        List<Player> players = new ArrayList();
-        for(World world:Bukkit.getWorlds()){
-            players.addAll(world.getPlayers());
-        }
-        return players;
+        return Players;
     }
 }
