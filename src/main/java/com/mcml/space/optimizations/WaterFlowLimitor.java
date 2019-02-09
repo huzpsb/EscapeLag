cb641b6c464d6bb6e963130f2b55d4897bf99aac
Vlele <1207223090@qq.com>
2019-02-09 19:28:22 +0800

    BUG修复：1.13/- 兼容

--- src/main/java/com/mcml/space/optimizations/WaterFlowLimitor.java
+++ src/main/java/com/mcml/space/optimizations/WaterFlowLimitor.java
@@ -1,6 +1,5 @@
 package com.mcml.space.optimizations;
 
-import static com.mcml.space.util.VersionLevel.modernApi;
 import static org.bukkit.Material.*;
 
 import java.util.HashMap;
@@ -16,42 +15,39 @@ import com.mcml.space.config.Optimizes;
 import com.mcml.space.core.EscapeLag;
 
 public class WaterFlowLimitor implements Listener {
-	
-	private final static HashMap<Chunk, Long> ChunkLastTime = new HashMap<Chunk, Long>();
-	private final static HashMap<Chunk, Integer> CheckedTimes = new HashMap<Chunk, Integer>();
-	
-	public WaterFlowLimitor(){
-		Bukkit.getScheduler().runTaskTimer(EscapeLag.plugin, CheckedTimes::clear, 7 * 20, 7 * 20);
-	}
-	
-	@EventHandler
-    public void WaterFowLimitor(BlockFromToEvent event) {
-		if(Optimizes.WaterFlowLimitorenable == true){
-			Block block = event.getBlock();
-			Chunk chunk = block.getChunk();
-	        if (modernApi() ?
-	                block.getType() == LEGACY_STATIONARY_WATER || block.getType() == LEGACY_STATIONARY_LAVA :
-	                block.getType() == STATIONARY_WATER || block.getType() == STATIONARY_LAVA) {
-	            if(CheckFast(block.getChunk())){
-	            	if(CheckedTimes.get(chunk) == null){
-	        			CheckedTimes.put(chunk, 0);
-	        		}
-	        		CheckedTimes.put(chunk, CheckedTimes.get(chunk) + 1);
-	        		if(CheckedTimes.get(chunk) > Optimizes.WaterFlowLimitorPerChunkTimes){
-	        			event.setCancelled(true);
-	        		}
-	            }else{
-	                ChunkLastTime.put(block.getChunk(), System.currentTimeMillis());
-	            }
-	        }
-		}
+
+    private final static HashMap<Chunk, Long> ChunkLastTime = new HashMap<Chunk, Long>();
+    private final static HashMap<Chunk, Integer> CheckedTimes = new HashMap<Chunk, Integer>();
+
+    public WaterFlowLimitor() {
+        Bukkit.getScheduler().runTaskTimer(EscapeLag.plugin, CheckedTimes::clear, 7 * 20, 7 * 20);
     }
-	
-	private static boolean CheckFast(Chunk chunk) {
+
+    @EventHandler
+    public void WaterFowLimitor(BlockFromToEvent event) {
+        if (Optimizes.WaterFlowLimitorenable == true) {
+            Block block = event.getBlock();
+            Chunk chunk = block.getChunk();
+            if (block.getType() == STATIONARY_WATER || block.getType() == STATIONARY_LAVA) {
+                if (CheckFast(block.getChunk())) {
+                    if (CheckedTimes.get(chunk) == null) {
+                        CheckedTimes.put(chunk, 0);
+                    }
+                    CheckedTimes.put(chunk, CheckedTimes.get(chunk) + 1);
+                    if (CheckedTimes.get(chunk) > Optimizes.WaterFlowLimitorPerChunkTimes) {
+                        event.setCancelled(true);
+                    }
+                } else {
+                    ChunkLastTime.put(block.getChunk(), System.currentTimeMillis());
+                }
+            }
+        }
+    }
+
+    private static boolean CheckFast(Chunk chunk) {
         if (ChunkLastTime.containsKey(chunk)) {
             return (ChunkLastTime.get(chunk).longValue() + 50L > System.currentTimeMillis());
         }
         return false;
     }
 }
-
