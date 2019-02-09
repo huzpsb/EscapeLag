6d1a8b1062a2f5ebc7227551442199a599f79eb7
Vlele <1207223090@qq.com>
2019-02-09 18:59:45 +0800

    Revert "。。"
    This reverts commit 1e106ecc71b343155803da86c8d8544c6b8b0b74.

--- src/main/java/com/mcml/space/optimizations/WaterFlowLimitor.java
+++ src/main/java/com/mcml/space/optimizations/WaterFlowLimitor.java
@@ -16,39 +16,42 @@ import com.mcml.space.config.Optimizes;
 import com.mcml.space.core.EscapeLag;
 
 public class WaterFlowLimitor implements Listener {
-
-    private final static HashMap<Chunk, Long> ChunkLastTime = new HashMap<Chunk, Long>();
-    private final static HashMap<Chunk, Integer> CheckedTimes = new HashMap<Chunk, Integer>();
-
-    public WaterFlowLimitor() {
-        Bukkit.getScheduler().runTaskTimer(EscapeLag.plugin, CheckedTimes::clear, 7 * 20, 7 * 20);
-    }
-
-    @EventHandler
+	
+	private final static HashMap<Chunk, Long> ChunkLastTime = new HashMap<Chunk, Long>();
+	private final static HashMap<Chunk, Integer> CheckedTimes = new HashMap<Chunk, Integer>();
+	
+	public WaterFlowLimitor(){
+		Bukkit.getScheduler().runTaskTimer(EscapeLag.plugin, CheckedTimes::clear, 7 * 20, 7 * 20);
+	}
+	
+	@EventHandler
     public void WaterFowLimitor(BlockFromToEvent event) {
-        if (Optimizes.WaterFlowLimitorenable == true) {
-            Block block = event.getBlock();
-            Chunk chunk = block.getChunk();
-            if (block.getType() == STATIONARY_WATER || block.getType() == STATIONARY_LAVA) {
-                if (CheckFast(block.getChunk())) {
-                    if (CheckedTimes.get(chunk) == null) {
-                        CheckedTimes.put(chunk, 0);
-                    }
-                    CheckedTimes.put(chunk, CheckedTimes.get(chunk) + 1);
-                    if (CheckedTimes.get(chunk) > Optimizes.WaterFlowLimitorPerChunkTimes) {
-                        event.setCancelled(true);
-                    }
-                } else {
-                    ChunkLastTime.put(block.getChunk(), System.currentTimeMillis());
-                }
-            }
-        }
+		if(Optimizes.WaterFlowLimitorenable == true){
+			Block block = event.getBlock();
+			Chunk chunk = block.getChunk();
+	        if (modernApi() ?
+	                block.getType() == LEGACY_STATIONARY_WATER || block.getType() == LEGACY_STATIONARY_LAVA :
+	                block.getType() == STATIONARY_WATER || block.getType() == STATIONARY_LAVA) {
+	            if(CheckFast(block.getChunk())){
+	            	if(CheckedTimes.get(chunk) == null){
+	        			CheckedTimes.put(chunk, 0);
+	        		}
+	        		CheckedTimes.put(chunk, CheckedTimes.get(chunk) + 1);
+	        		if(CheckedTimes.get(chunk) > Optimizes.WaterFlowLimitorPerChunkTimes){
+	        			event.setCancelled(true);
+	        		}
+	            }else{
+	                ChunkLastTime.put(block.getChunk(), System.currentTimeMillis());
+	            }
+	        }
+		}
     }
-
-    private static boolean CheckFast(Chunk chunk) {
+	
+	private static boolean CheckFast(Chunk chunk) {
         if (ChunkLastTime.containsKey(chunk)) {
             return (ChunkLastTime.get(chunk).longValue() + 50L > System.currentTimeMillis());
         }
         return false;
     }
 }
+
