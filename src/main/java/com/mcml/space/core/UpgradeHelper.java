ba2e78c728bf286b2f2d987d6a3899f8a7a9aa0b
Vlele <1207223090@qq.com>
2019-03-10 19:05:09 +0800

    修复1.7.10 的报错（才发现？）

--- src/main/java/com/mcml/space/core/UpgradeHelper.java
+++ src/main/java/com/mcml/space/core/UpgradeHelper.java
@@ -5,13 +5,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 
 import com.mcml.space.config.Core;
-import com.mcml.space.util.AzureAPI;
-import com.mcml.space.util.Locale;
 import com.mcml.space.util.Perms;
-import com.mcml.space.util.VersionLevel;
 import lombok.Setter;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -21,22 +16,23 @@ public class UpgradeHelper implements Listener {
 
     @Setter
     private static boolean isKnown;
-    private static TextComponent component;
+//    private static TextComponent component;
 
     public static void init(Plugin plugin) {
         if (Core.AutoUpdate) {
             return;
         }
-        if (VersionLevel.isHigherEquals(VersionLevel.Version.MINECRAFT_1_8_R1)) {
-            component = new TextComponent(Locale.isNative()
-                    ? "§3§lE§b§lL §b> §f输入 §3/el updateon §f开启自动更新, 永远保持高效运作! "
-                    : "§3§lE§b§lL §b> §fType §3/el updateon §fto enable auto-update, always keep running efficiently! ");
-            TextComponent knownOption = new TextComponent("§b".concat(Locale.isNative() ? "我知道了" : "Got it"));
-            knownOption.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/el iknown"));
-            component.addExtra(knownOption);
-            Bukkit.getPluginManager().registerEvents(new UpgradeHelper(), plugin);
-            AzureAPI.log(Locale.isNative() ? "核心模块 - 升级助手 已启动" : "Coremodule - UpgradeHelper has been enabled");
-        }
+//        if (VersionLevel.isHigherEquals(VersionLevel.Version.MINECRAFT_1_8_R1)) {
+//            component = new TextComponent(Locale.isNative()
+//                    ? "§3§lE§b§lL §b> §f输入 §3/el updateon §f开启自动更新, 永远保持高效运作! "
+//                    : "§3§lE§b§lL §b> §fType §3/el updateon §fto enable auto-update, always keep running efficiently! ");
+//            TextComponent knownOption = new TextComponent("§b".concat(Locale.isNative() ? "我知道了" : "Got it"));
+//            knownOption.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/el iknown"));
+//            component.addExtra(knownOption);
+//            
+//            AzureAPI.log(Locale.isNative() ? "核心模块 - 升级助手 已启动" : "Coremodule - UpgradeHelper has been enabled");
+//        }
+        Bukkit.getPluginManager().registerEvents(new UpgradeHelper(), plugin);
     }
 
     @EventHandler
@@ -46,7 +42,7 @@ public class UpgradeHelper implements Listener {
         }
         Player player = evt.getPlayer();
         if (Perms.has(player)) {
-            player.sendMessage(component);
+            player.sendMessage("§3§lE§b§lL §b> §f输入 §3/el updateon §f开启自动更新, 永远保持高效运作! ");
         }
     }
 }
