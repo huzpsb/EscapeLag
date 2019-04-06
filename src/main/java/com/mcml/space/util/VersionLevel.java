d595bd29731697639be5fa586c89086038f951aa
Vlele <1207223090@qq.com>
2019-04-06 21:55:53 +0800

    Cat服务端分辨

--- src/main/java/com/mcml/space/util/VersionLevel.java
+++ src/main/java/com/mcml/space/util/VersionLevel.java
@@ -73,7 +73,7 @@ public class VersionLevel {
     private static void checkServerType(String rawVersion) {
         String bukkitVersion = rawVersion.toLowerCase();
         boolean thermos = bukkitVersion.contains("thermos") || bukkitVersion.contains("contigo");
-        forge = thermos || bukkitVersion.contains("cauldron") || bukkitVersion.contains("mcpc") || bukkitVersion.contains("uranium");
+        forge = thermos || bukkitVersion.contains("cauldron") || bukkitVersion.contains("mcpc") || bukkitVersion.contains("uranium")||bukkitVersion.contains("cat");
         paper = bukkitVersion.contains("paper") || bukkitVersion.contains("taco") || bukkitVersion.contains("torch") || bukkitVersion.contains("akarin");
         if (paper || thermos || bukkitVersion.contains("spigot")) spigot = true;
     }
